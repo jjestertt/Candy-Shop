76c82981b4fdbbb20e4af89b3b1329750ecc4d97
Sivak Max <66674521+jjestertt@users.noreply.github.com>
2021-07-09 02:07:29 +0200

    added bg animation

--- js/main.js
+++ js/main.js
@@ -46,3 +46,80 @@ menuLinks.forEach(item => {
 
 menu.addEventListener('click', handleMenuClick)
 logoBlock.addEventListener('click', handleMenuClick)
+
+
+
+particlesJS('particles-js',
+
+   {
+      "particles": {
+         "number": {
+            "value": 80,
+            "density": {
+               "enable": true,
+               "value_area": 800
+            }
+         },
+         "color": {
+            "value": "#FDFFFC"
+         },
+         "shape": {
+            "type": "circle",
+            "stroke": {
+               "width": 0,
+               "color": "#000000"
+            },
+            "polygon": {
+               "nb_sides": 5
+            },
+            "image": {
+               "src": "img/github.svg",
+               "width": 100,
+               "height": 100
+            }
+         },
+         "opacity": {
+            "value": 0.2,
+            "random": true,
+            "anim": {
+               "enable": false,
+               "speed": 1,
+               "opacity_min": 0.1,
+               "sync": false
+            }
+         },
+         "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+               "enable": true,
+               "speed": 1,
+               "size_min": 3,
+               "sync": false
+            }
+         },
+         "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+         },
+         "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+               "enable": false,
+               "rotateX": 600,
+               "rotateY": 1200
+            }
+         }
+      },
+      "retina_detect": true,
+   }
+
+);
